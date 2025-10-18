2e58b7c1792feada4a36699dc98d42d864a10d33
dergachevwvadim <vadim.valentinovic@mail.ru>
2025-10-18 15:53:16 +0300

    Initial Commit

---
+++ ImageFeedTests/ImageFeedTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFeedTests.swift
+//  ImageFeedTests
+//
+//  Created by User on 18.10.2025.
+//
+
+import Testing
+@testable import ImageFeed
+
+struct ImageFeedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
